bfc4aa9f3570ba0d7db2830fbc2e442ce96f6994
Jon Phenow <j.phenow@gmail.com>
2013-06-30 22:28:55 -0500

    add bootstrap gem and its assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require twitter/bootstrap
+//= require twitter/bootstrap/bootstrap-transition
+//= require twitter/bootstrap/bootstrap-alert
+//= require twitter/bootstrap/bootstrap-modal
+//= require twitter/bootstrap/bootstrap-button
+//= require twitter/bootstrap/bootstrap-collapse
 //= require turbolinks
 //= require_tree .
